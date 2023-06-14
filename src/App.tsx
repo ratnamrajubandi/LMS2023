51faf1c2927241bc91cd163d1d62c1f3921856ea
Ratnam Raju Bandi <ratnamrajubandi@gmail.com>
2023-06-14 16:46:25 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-purple-200 flex justify-center items-center">
+      <h1 className="text-3xl text-blue-600 font-bold text-center">
+        Installed & Setup Vite + React + Typescript + Tailwind CSS 3
+      </h1>
+    </div>
+  );
+}
+
+export default App;
